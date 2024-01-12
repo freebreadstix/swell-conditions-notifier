842c65995db14299b5253056e4be522cfee9060f
Luke Nguyen <lukenguyen@Lukes-MacBook-Pro.local>
2024-01-11 17:10:36 -0800

    Add fx to format tide to msg

--- request.py
+++ request.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import pandas as pd
 import requests
 
-def get_tide(station=9413450, num_days=3):
+def get_tide(station=9413450, num_days=3, interval=10):
     product, datum, time_zone, units = "predictions", "MLLW", "lst_ldt", "english"
     data_format = "json"
 
@@ -42,7 +42,7 @@ def get_tide(station=9413450, num_days=3):
     df = pd.DataFrame(predictions)
 
     # Get data by hour
-    df = df.iloc[::10]
+    df = df.iloc[::interval]
 
     df['dt_object'] = df['t'].apply(lambda x: pd.to_datetime(x).to_pydatetime())
     df['day'] = df['dt_object'].apply(lambda x: calendar.day_name[x.weekday()])
@@ -53,12 +53,25 @@ def get_tide(station=9413450, num_days=3):
 
     # Filter by ideal tide period
     df['v'] = df['v'].astype('float')
-    df = df[df['v'] < 3.5]
+    # df = df[df['v'] < 3.5]
 
-    return df[['day', 'time', 'v']]
+    return df[['day', 'time', 'v']], begin_date, end_date
+
+def format_tide(tide, begin_date):
+    s = f"\nHeres you're tide report:\n\n"
+
+    current_day = ''
+    for idx, row in tide.iterrows():
+        if current_day != row['day']:
+            current_day = row['day']
+            s += f"{row['day']}\n"
+        s += f"{row['time']} || {row['v']}\n"
+        
+    return s    
 
 if __name__ == "__main__":
     station = 9413450 # Monterey
     num_days = 3
-    df = get_tide(station, num_days)
-    print(df)
+    df, begin_date, _ = get_tide(station, num_days, interval=20)
+    msg = format_tide(df, begin_date)
+    print(msg)
